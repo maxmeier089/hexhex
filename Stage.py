0b19ee7d45e07299bb8b8d6eae4ebeefbdaadaeb
Max Meier <max.meier.089@googlemail.com>
2025-01-14 16:10:52 +0100

    cln

--- Stage.py
+++ Stage.py
@@ -4,30 +4,29 @@ from pygame import Vector2
 from Config import *
 
 class Stage:
+
+    TILE_SIZE = 8
+
     def __init__(self, groundTiles):
         self.groundTiles = groundTiles
 
-        self.rows = int(SCREEN_HEIGHT / 8)
-        self.cols = int(SCREEN_WIDTH / 8)
+        self.rows = int(SCREEN_HEIGHT / Stage.TILE_SIZE)
+        self.cols = int(SCREEN_WIDTH / Stage.TILE_SIZE)
 
-        lst = []
         self.groundMatrix = []
         for _ in range(self.cols):
+            currentColumn = []
             for _ in range(self.rows):
-                lst.append(random.randint(0, len(groundTiles)-1))
-            self.groundMatrix.append(lst)
-            lst = []
-
-        self.name = "x"
-
-        
+                currentColumn.append(random.randint(0, len(groundTiles) - 1))
+            self.groundMatrix.append(currentColumn)    
 
 
     def draw(self, displaysurface, pos):
         for x in range(self.cols):
             for y in range(self.rows):
                 tileNumber = self.groundMatrix[x][y]
-                displaysurface.blit(self.groundTiles[tileNumber], Vector2(x * 8, y * 8))
+                displaysurface.blit(self.groundTiles[tileNumber], Vector2(x * Stage.TILE_SIZE, y * Stage.TILE_SIZE))
+                
 
     def loadTiles(self, path):
         spritesheet = pygame.image.load(path).convert_alpha()
